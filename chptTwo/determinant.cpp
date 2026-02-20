e1242fe8b941ff650f92d3d57d4154f3a6b3facf
Daltonshults <Dshults@cocc.edu>
2026-02-20 14:34:21 -0800

    Incorporated matrix class.

--- chptTwo/determinant.cpp
+++ chptTwo/determinant.cpp
@@ -1,30 +1,29 @@
-#include <iostream>
 
-void populateFromList(int** matrix, int ints[], int rows=4, int cols=4) {
-    int num = 0;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix[i][j] = ints[num];
-            num++;
-        }
-    }
+#include "determinant.h"
+
+Determinant::Determinant(int rows, int cols, int* ints)
+    :matrix(nullptr), rows(rows), cols(cols), ints(ints)
+{
+    matrix = m.allocate(rows, cols);
+    matrix = m.populateFromList(matrix, ints, rows, cols);
+    det = fourByFourDet(matrix); 
 }
 
-int** allocate(int rows, int cols) {
-    int **matrix = (int**) malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int*) malloc(cols * sizeof(int*));
-    }
-
-    return matrix;
+int Determinant::getDet() {
+    return this->det;
 }
 
-int twoByTwoDet(int** matrix) {
+int** Determinant::getMatrix() {
+    return this->matrix;
+}
+
+int Determinant::twoByTwoDet(int** matrix) {
     return (int) ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]));
 }
-void populateMats(int*** mats, int** matrix, int indexes) {
+void Determinant::populateMats(int*** mats, int** matrix, int indexes) {
+
     for (int i = 0; i < indexes; i ++) {
-        mats[i] = allocate(indexes-1, indexes-1);
+        mats[i] = m.allocate(indexes-1, indexes-1);
     }
 
     int currentI = 0;
@@ -64,7 +63,7 @@ void populateMats(int*** mats, int** matrix, int indexes) {
     }
 }
 
-int threeByThreeDet(int** matrix) {
+int Determinant::threeByThreeDet(int** matrix) {
     int** mats[3];
     populateMats(mats, matrix, 3);
 
@@ -73,7 +72,7 @@ int threeByThreeDet(int** matrix) {
         + matrix[0][2] * twoByTwoDet(mats[2]); 
 }
 
-int fourByfourDet(int** matrix) {
+int Determinant::fourByFourDet(int** matrix) {
     int** mats[4];
     populateMats(mats, matrix, 4);
 
@@ -81,13 +80,4 @@ int fourByfourDet(int** matrix) {
         - matrix[0][1] * threeByThreeDet(mats[1]) 
         + matrix[0][2] * threeByThreeDet(mats[2]) 
         - matrix[0][3] * threeByThreeDet(mats[3]); 
-}
-
-void printMatrix(int** matrix, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            std::cout << matrix[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
 }
