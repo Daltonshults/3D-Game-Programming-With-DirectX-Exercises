147703437a2d97795f24d606cbebd95430b692af
Daltonshults <Dshults@cocc.edu>
2026-02-20 14:35:15 -0800

    Incorporated matrix, and removed duplicated functions.

--- chptTwo/determinant.h
+++ chptTwo/determinant.h
@@ -1,15 +1,26 @@
 #include <iostream>
-
+#include "matrix.h"
 class Determinant {
 
-    Determinant();
-    ~Determinant();
+public:
+    Determinant(int rows, int cols, int* ints);
+    ~Determinant() = default;
 
     void run();
 
+    int getDet();
+    int** getMatrix();
+
 private:
-    void populateFromList(int** matrix, int ints[], int rows, int cols);
-    int** allocate(int rows, int cols);
+    Matrix m;
+    int** matrix;
+    int rows;
+    int cols;
+    int *ints;
+    int det;
+
+    // void populateFromList(int** matrix, int ints[], int rows, int cols);
+    // int** allocate(int rows, int cols);
     int twoByTwoDet(int** matrix);
     void populateMats(int*** mats, int** matrix, int indexes);
     int threeByThreeDet(int** matrix);
