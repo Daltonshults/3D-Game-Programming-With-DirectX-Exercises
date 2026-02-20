0ae9bf7344e2047ea1a3baace8889963cfdec176
Daltonshults <Dshults@cocc.edu>
2026-02-20 14:35:45 -0800

    Moved functionality to determinant class.

--- chptTwo/determinantAndInverseMain.cpp
+++ chptTwo/determinantAndInverseMain.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-#include "transpose.h"
+#include "determinant.h"
+#include "matrixPrinter.h"
 
 int main(int argc, char** argv) {
-
     int ints[] = {1, 3, 5, 9, 1, 3, 1, 7, 4, 3, 9, 7, 5, 2, 0, 9};
-    int** matrix = allocate(4, 4);
-    populateFromList(matrix, ints, 4, 4);
-
-    printMatrix(matrix, 4, 4);
-    int fourDet = fourByfourDet(matrix);
-    std::cout << fourDet << "\n";
-
-
-
+    Determinant d(4, 4, ints);
+    MatrixPrinter m;
+    m.printMatrix(d.getMatrix(), 4, 4);
+    std::cout << d.getDet() << "\n";
     return 0;
 }
