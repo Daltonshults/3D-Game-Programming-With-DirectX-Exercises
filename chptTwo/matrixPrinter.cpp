f57a42e7f30abc58bb8a05c0e534bd237fc382d1
Daltonshults <Dshults@cocc.edu>
2026-02-20 13:30:57 -0800

    Created matrix printer class.

---
+++ chptTwo/matrixPrinter.cpp
@@ -0,0 +1,10 @@
+#include "matrixPrinter.h"
+
+void MatrixPrinter::printMatrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
---
+++ chptTwo/matrixPrinter.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class MatrixPrinter {
+
+public:
+    MatrixPrinter() = default;
+    ~MatrixPrinter() = default;
+
+    void printMatrix(int** matrix, int rows, int cols);
+};
